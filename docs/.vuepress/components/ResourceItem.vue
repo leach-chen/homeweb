d0aa8d05105d69296f3eb21807eb3386cd5abfa5
LAPTOP-8CFJ92M4\34976 <349763031@qq.com>
2019-02-16 19:37:11 +0800

    modify

--- docs/.vuepress/components/ResourceItem.vue
+++ docs/.vuepress/components/ResourceItem.vue
@@ -3,82 +3,103 @@
 <div class="box-content clearfix">
 
        <el-card class="box-card">
-          <div>
-              <div id = "imgfloatdiv">
-                  <p>aaaaaaaaaa</p>
+          <div class="imgfloatcontent" >
+              <div class = "imgfloatdiv">  
+                  <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
               </div>
-              <img id = "imgfloat" src="../public/img/bk1.jpg"/>
+              <img src="../public/img/bk1.jpg"/>
           </div>
 
-           <el-row>
-                <el-button type="primary" @click="goContribute">贡献资源</el-button>
-            </el-row>
+         <div class="btn-content">
+            <el-button type="primary">预览效果</el-button>
+            <el-button type="primary">源码下载</el-button>
+         </div>
        </el-card>
 
        <el-card class="box-card">
-          <div>
-              <div id = "imgfloatdiv">
-                  <p>aaaaaaaaaa</p>
+          <div class="imgfloatcontent" >
+              <div class = "imgfloatdiv">  
+                  <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
               </div>
-              <img id = "imgfloat" src="../public/img/bk1.jpg"/>
+              <img src="../public/img/bk1.jpg"/>
           </div>
 
-           <el-row>
-                <el-button type="primary" @click="goContribute">贡献资源</el-button>
-            </el-row>
+         <div class="btn-content">
+            <el-button type="primary">预览效果</el-button>
+            <el-button type="primary">源码下载</el-button>
+         </div>
        </el-card>
 
-              <el-card class="box-card">
-          <div>
-              <div id = "imgfloatdiv">
-                  <p>aaaaaaaaaa</p>
+       <el-card class="box-card">
+          <div class="imgfloatcontent" >
+              <div class = "imgfloatdiv">  
+                  <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
               </div>
-              <img id = "imgfloat" src="../public/img/bk1.jpg"/>
+              <img src="../public/img/bk1.jpg"/>
           </div>
 
-           <el-row>
-                <el-button type="primary" @click="goContribute">贡献资源</el-button>
-            </el-row>
+         <div class="btn-content">
+            <el-button type="primary">预览效果</el-button>
+            <el-button type="primary">源码下载</el-button>
+         </div>
        </el-card>
 
-              <el-card class="box-card">
-          <div>
-              <div id = "imgfloatdiv">
-                  <p>aaaaaaaaaa</p>
+       <el-card class="box-card">
+          <div class="imgfloatcontent" >
+              <div class = "imgfloatdiv">  
+                  <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
               </div>
-              <img id = "imgfloat" src="../public/img/bk1.jpg"/>
+              <img src="../public/img/bk1.jpg"/>
           </div>
 
-           <el-row>
-                <el-button type="primary" @click="goContribute">贡献资源</el-button>
-            </el-row>
+         <div class="btn-content">
+            <el-button type="primary">预览效果</el-button>
+            <el-button type="primary">源码下载</el-button>
+         </div>
        </el-card>
 
-              <el-card class="box-card">
-          <div>
-              <div id = "imgfloatdiv">
-                  <p>aaaaaaaaaa</p>
+       <el-card class="box-card">
+          <div class="imgfloatcontent" >
+              <div class = "imgfloatdiv">  
+                  <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
               </div>
-              <img id = "imgfloat" src="../public/img/bk1.jpg"/>
+              <img src="../public/img/bk1.jpg"/>
           </div>
 
-           <el-row>
-                <el-button type="primary" @click="goContribute">贡献资源</el-button>
-            </el-row>
+         <div class="btn-content">
+            <el-button type="primary">预览效果</el-button>
+            <el-button type="primary">源码下载</el-button>
+         </div>
        </el-card>
 
-              <el-card class="box-card">
-          <div>
-              <div id = "imgfloatdiv">
-                  <p>aaaaaaaaaa</p>
+       <el-card class="box-card">
+          <div class="imgfloatcontent" >
+              <div class = "imgfloatdiv">  
+                  <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
               </div>
-              <img id = "imgfloat" src="../public/img/bk1.jpg"/>
+              <img src="../public/img/bk1.jpg"/>
           </div>
 
-           <el-row>
-                <el-button type="primary" @click="goContribute">贡献资源</el-button>
-            </el-row>
+         <div class="btn-content">
+            <el-button type="primary">预览效果</el-button>
+            <el-button type="primary">源码下载</el-button>
+         </div>
        </el-card>
+
+       <el-card class="box-card">
+          <div class="imgfloatcontent" >
+              <div class = "imgfloatdiv">  
+                  <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+              </div>
+              <img src="../public/img/bk1.jpg"/>
+          </div>
+
+         <div class="btn-content">
+            <el-button type="primary">预览效果</el-button>
+            <el-button type="primary">源码下载</el-button>
+         </div>
+       </el-card>
+
 
 </div>
 
@@ -91,8 +112,11 @@
       return {}
     },
     mounted () {
-      document.getElementById("imgfloatdiv").style.width= document.getElementById("imgfloat").width+"px";
-      document.getElementById("imgfloatdiv").style.height= document.getElementById("imgfloat").height+"px";
+        
+      //document.getElementById("imgfloatdiv").style.width= document.getElementById("imgfloat").width+"px";
+      //document.getElementById("imgfloatdiv").style.height= document.getElementById("imgfloat").height+"px";
+
+      //console.log(document.getElementById("imgfloat").width+"px")
     },
    
   }
@@ -108,20 +132,46 @@
     width: 29%;
     float: left;
     margin: 10px;
+
+    
 }
 
-#imgfloatdiv
+/* 相对布局相对其第一个relative的父布局进行定位，设置其bottom：0属性，父布局的高度由其它非相对定位布局的子元素决定*/
+
+.imgfloatcontent
+{
+    overflow: hidden;
+    position: relative;
+}
+
+.imgfloatdiv
 {
   position: absolute;
-
+  bottom:0px;
+  padding:0px;
+  margin:0px;
 }
 
-#imgfloatdiv p
+.imgfloatdiv p
 {
   /* position:absolute;
   bottom:0px;
   padding:0px;
   margin:0px */
+  color: aliceblue;
+  font-size: 18px;
+  
+  overflow: hidden;
+  text-overflow:ellipsis;
+  white-space: nowrap;
+
+}
+
+.btn-content
+{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
 }
 
 
