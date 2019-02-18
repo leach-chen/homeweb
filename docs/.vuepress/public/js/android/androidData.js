347adefe12b2add8dd62d3ad2baebfd52e877acd
leach.chen <leach>
2019-02-18 18:27:40 +0800

    modify

--- docs/.vuepress/public/js/android/androidData.js
+++ docs/.vuepress/public/js/android/androidData.js
@@ -1,7 +1,7 @@
 
 var host = "https://www.leachchen.com/h5website/"
 var androidDataList = [
-  {thumb: host+"001/thumb/demo.png",preview: host+"001/source/",download: host+"001/download/before.zip",description:"旅游风格的H5网站aa"},
+  {thumb: host+"001/thumb/demo.png",preview: host+"001/source/",download: host+"001/download/before.zip",description:"Android旅游风格的H5网站"},
 ];
   
   export{
