edb40ab8a32c23ad0d0af9967988edeffd0dcb67
leach.chen <leach>
2019-02-19 15:17:18 +0800

    modify

---
+++ docs/.vuepress/components/About.vue
@@ -0,0 +1,46 @@
+<template>
+
+  <el-container direction="vertical">
+    
+    <div class="commontent contentpart1">
+        <el-card class="box-card">
+            关于
+        </el-card>
+    </div>
+
+  </el-container>
+</template>
+
+
+
+<style lang="less" scoped>
+
+.commontent
+{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    overflow-x: hidden;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    min-height: 800px;
+}
+
+.contentpart1{
+    background: url("../public/img/services-bg.jpg");
+    background-attachment: fixed;
+}
+
+</style>
+
+<style>
+.el-card__body {
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  justify-content: center;
+ 
+}
+</style>
