b766e087c0a7cfb268ce0078ce9813dbba638fdb
leach.chen <leach>
2019-03-06 18:45:28 +0800

    modify

--- docs/.vuepress/public/js/data/VueData.js
+++ docs/.vuepress/public/js/data/VueData.js
@@ -10,7 +10,7 @@ var adminurl =""
 var fullVueDataList = [
   {
     // thumbUrl: require("../../img/vue/vue1.jpg"),
-    thumbUrl: host+"1/storevue1_1_t_20190303.jpg",
+    thumbUrl: host+"1/storevue1_1_t_401.jpg",
     previewUrl: "http://panjiachen.github.io/vue-element-admin",
     downloadUrl: "https://codeload.github.com/PanJiaChen/vue-element-admin/zip/master",
     description:"完整的Vue后台管理系统",
